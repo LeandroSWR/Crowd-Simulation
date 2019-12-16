4b981a97ffae0dd3f1710bb08c40ff1c48f85398
fs000 <flaviosantos002010@hotmail.com>
2019-12-16 16:23:04 +0000

    Add functional explosions

--- Crowd Simulation/Assets/Scripts/ExplosionBehaviour.cs
+++ Crowd Simulation/Assets/Scripts/ExplosionBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class ExplosionBehaviour : MonoBehaviour {
@@ -12,9 +13,13 @@ public class ExplosionBehaviour : MonoBehaviour {
     private float killRadius;  // The radius at which NPC's get killed
     private float stunRadius;  // The radius at which NPC's get stunned
     private float panicRadius; // The radius at which NPC's start panicking
+    private float maxRadius;   // The radius at which this explosion gets destroyed
 
     // The scale to be applied
     private Vector3 desiredScale;
+
+    // The current explosion radius
+    private float currentRadius;
 
     private void Awake() {
 
@@ -26,8 +31,8 @@ public class ExplosionBehaviour : MonoBehaviour {
 
     void Update() {
 
-        // Change scale every frame
-        Explode();
+        // Spread the fire / Change scale
+        SpreadFire();
     }
 
     /// <summary>
@@ -39,27 +44,81 @@ public class ExplosionBehaviour : MonoBehaviour {
     /// <param name="stunRadius">The radius to stun NPCs</param>
     /// <param name="panicRadius">The radius for the NPCs to panic</param>
     public void AssignVariables(float explosionSpeed, float fireSpeed, 
-        float killRadius, float stunRadius, float panicRadius) {
+        float killRadius, float stunRadius, float panicRadius, float maxRadius) {
 
         this.explosionSpeed = explosionSpeed;
         this.fireSpeed = fireSpeed;
         this.killRadius = killRadius;
         this.stunRadius = stunRadius;
         this.panicRadius = panicRadius;
+        this.maxRadius = maxRadius;
     }
 
     /// <summary>
     /// Start the explosion "animation"
     /// </summary>
-    /// <param name="explosion">The explosion object</param>
+    /// <param name = "explosion" > The explosion object</param>
     /// <returns></returns>
-    private void Explode() {
+    private void SpreadFire() {
 
-        if (desiredScale.x >= killRadius) return;
+        // Save this explosion radius
+        currentRadius = desiredScale.x;
 
-        desiredScale += Vector3.one * (Time.deltaTime * explosionSpeed);
+        // If our current radius is less than the one to kill NPCs...
+        if (currentRadius < killRadius) {
+
+            // ...Increase the radius with the appropriate speed
+            desiredScale += Vector3.one * (Time.deltaTime * explosionSpeed);
+
+        // If the current radius is bigger or equals the radius to kill NPCs
+        // but it's smaller than the maximum one...
+        } else if (currentRadius >= killRadius && currentRadius < maxRadius) {
+
+            // ...Increase the radius with the appropriate speed
+            desiredScale += Vector3.one * (Time.deltaTime * fireSpeed);
+
+        // If the current radius is bigger or equals the maximum radius...
+        } else if (currentRadius >= maxRadius) {
+
+            // ...Destroy this explosion GameObject
+            Destroy(gameObject);
+        }
+
+        // Apply the correct scale on the 'y' axis (by resetting it)
         desiredScale.y = transform.localScale.y;
 
+        // Apply the new scale to this explosion GameObject
         transform.localScale = desiredScale;
+    }
+
+    /// <summary>
+    /// Creates an explosion at the center of the Instantiated Explosion
+    /// </summary>
+    public void Explode() {
+
+        // Gets all colliders around this GameObject
+        Collider[] npcs = Physics.OverlapSphere(transform.position, killRadius);
+
+        // Iterates through all Colliders found...
+        foreach(Collider c in npcs) {
+
+            // ...finds the ones with the 'NPC' tag...
+            if (c.CompareTag("NPC")) {
+
+                // ...disables Behaviours and AI based movement...
+                c.GetComponent<NPCBehaviour>().enabled = false;
+                c.GetComponent<NavMeshAgent>().enabled = false;
+
+                // ...fetches the colliders' Rigidbody...
+                Rigidbody otherRb = c.GetComponent<Rigidbody>();
+
+                // ...changes rigidbody settings so that Physics can work on them...
+                otherRb.isKinematic = false;
+                otherRb.useGravity = true;
+
+                // ...and adds an explosion force based on the center of the explosion
+                otherRb.AddExplosionForce(100000, transform.position, killRadius, 50000);
+            }
+        }
     }
 }
--- Crowd Simulation/Assets/Scripts/ExplosionManager.cs
+++ Crowd Simulation/Assets/Scripts/ExplosionManager.cs
@@ -15,17 +15,13 @@ public class ExplosionManager : MonoBehaviour {
     [SerializeField] private float killRadius;  // The radius at which NPC's get killed
     [SerializeField] private float stunRadius;  // The radius at which NPC's get stunned
     [SerializeField] private float panicRadius; // The radius at which NPC's start panicking
+    [SerializeField] private float maxRadius;   // The radius at which the Explosions stop
 
     // The explosion's prefab object
     [SerializeField] private Transform explosionPrefab;
 
     // The agents' parent object
     [SerializeField] private Transform agentsParent;
-
-    void Start() {
-
-
-    }
 
     void Update() {
 
@@ -37,8 +33,10 @@ public class ExplosionManager : MonoBehaviour {
     /// </summary>
     private void CheckInput() {
 
+        // Detect the wanted button press
         if (Input.GetKeyDown(explosionKey)) {
 
+            // Select a random NPC to Instantiate an explosion
             SelectTerrorist();
         }
     }
@@ -51,11 +49,16 @@ public class ExplosionManager : MonoBehaviour {
         // Get a random index based on the number of NPCs
         int npcIndex = Random.Range(0, 500);
 
+        // Get the 'ExplosionBehaviour' script attached to the newly Instantiated explosion GameObject
         ExplosionBehaviour eb = 
             Instantiate(explosionPrefab,
             agentsParent.GetChild(npcIndex).position,
             Quaternion.identity).GetComponent<ExplosionBehaviour>();
 
-        eb.AssignVariables(explosionSpeed, fireSpeed, killRadius, stunRadius, panicRadius);
+        // Pass variables from this script towards the last explosion
+        eb.AssignVariables(explosionSpeed, fireSpeed, killRadius, stunRadius, panicRadius, maxRadius);
+
+        // Activate the explosion
+        eb.Explode();
     }
 }
