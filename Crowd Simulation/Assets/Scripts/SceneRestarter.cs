76834a0c0396e129bda53698f8792e7343667fd6
fs000 <flaviosantos002010@hotmail.com>
2019-12-17 11:06:55 +0000

    Create restart method

---
+++ Crowd Simulation/Assets/Scripts/SceneRestarter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRestarter : MonoBehaviour {
+
+    public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
